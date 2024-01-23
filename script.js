3e9943d8a58c2d0c300e99b468cfafcb9ae960ad
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-23 11:26:18 +0100

    updated script

--- script.js
+++ script.js
@@ -19,47 +19,88 @@ function complete() {
 
 function newQuote() {
   loading();
-  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
-  // Check if author is available
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+
   if (!quote.author) {
-    authorText.textContent = "Unknown";
+    quoteText.textContent = "Unknown";
+    console.log("Author not found");
   } else {
     authorText.textContent = quote.author.split(",")[0];
   }
 
-  if (quote.text.length > 100) {
+  if (quote.length > 30) {
     quoteText.classList.add("long-quote");
   } else {
     quoteText.classList.remove("long-quote");
   }
 
-  //   Set quote, Hide loader
   quoteText.textContent = quote.text;
+
   complete();
 }
 
 async function getQuotes() {
   loading();
-  const apiUrl = "https://type.fit/api/quotes";
+
+  const url = "https://type.fit/api/quotes";
 
   try {
-    const response = await fetch(apiUrl);
-    apiQuotes = await response.json();
+    let response = await fetch(url);
+    response = await response.json();
+    // console.log(response);
+    apiQuotes = response;
     newQuote();
   } catch (error) {
-    // Catch Error
-    console.log(error);
+    console.log("Error: " + error);
   }
 }
+
+// function newQuote() {
+//   loading();
+//   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+//   // Check if author is available
+//   if (!quote.author) {
+//     authorText.textContent = "Unknown";
+//   } else {
+//     authorText.textContent = quote.author.split(",")[0];
+//   }
+
+//   if (quote.text.length > 100) {
+//     quoteText.classList.add("long-quote");
+//   } else {
+//     quoteText.classList.remove("long-quote");
+//   }
+
+//   // //   //   Set quote, Hide loader
+//   quoteText.textContent = quote.text;
+//   complete();
+// }
 
 function tweetQuote() {
   const twitterUrl = `https://twitter.com/intent/tweet=${quoteText.textContent} - ${authorText.textContent}`;
   window.open(twitterUrl, "_blank");
 }
 
+// Event Listeners
 newQuoteBtn.addEventListener("click", newQuote);
-
 twitterBtn.addEventListener("click", tweetQuote);
-
 getQuotes();
+
+// function getQuotes() {
+//   fetch("https://type.fit/api/quotes")
+//     .then((res) => res.json())
+//     .then((data) => {
+//       const quote = data[Math.floor(Math.random() * data.length)];
+//       quoteText.textContent = quote.text;
+//       authorText.textContent = quote.author.split(",")[0];
+//     });
+// }
+
+// getQuotes();
+
+// newQuoteBtn.addEventListener("click", getQuotes);
+
+///////////////////////////////
